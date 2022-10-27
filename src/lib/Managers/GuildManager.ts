ab021b771ea39a5b351b559e1f4b5cc5879a9953
Lars de Kooter <de.kooter.lars@gmail.com>
2022-10-27 15:21:33 +0200

    cleanup

--- src/lib/Managers/GuildManager.ts
+++ src/lib/Managers/GuildManager.ts
@@ -3,8 +3,6 @@ import { Manager } from "./Manager.js";
 
 export class GuildManager extends Manager<string, Guild> {
   async fetch(id: string, force = false): Promise<Guild | null> {
-    const oldGuild =
-      this.client.guilds.cache.get(id) ?? this.client.partialGuilds.get(id);
     if (force) {
       const guild = new Guild(
         await this.client.rest.get(`/guilds/${id}?with_counts=true`),
